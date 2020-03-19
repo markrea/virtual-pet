85b8db8b3b3560399f8b3baf359246fb9cd0b265
Mark Rea <markrea366@gmail.com>
2020-03-19 09:44:16 +0000

    added fitness and hunger attributes

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,24 +4,35 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
-
-  describe('constructor', () => {
   
     it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
-    });
+        const pet = new Pet('Fido');
+    
+        expect(pet.name).toEqual('Fido');
+      });
+    
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.age).toEqual(0);
+      });
+    
+    it('has a initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.hunger).toEqual(0);
+      });
+    
+    it('has a initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.fitness).toEqual(10);
+      });
   });
 
-  describe('constructor', () => {
-    it('has a initial age of 0', () => {
-      const pet = new Pet('Fido');
   
-      expect(pet.age).toEqual(0);
-    });
-  });
+
+ 
 
   describe('growUp', () => {
     it('increments the age by 1', () => {
@@ -31,4 +42,21 @@ describe('constructor', () => {
   
       expect(pet.age).toEqual(1);
     });
+   
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.hunger).toEqual(5);
+      });
+    
+    it('decrements the fitness by 3', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.fitness).toEqual(7);
+      });
   });
+
