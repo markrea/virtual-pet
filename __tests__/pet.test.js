4bac2cc3fa4e4aa212de38005f70646a901c3783
Mark Rea <markrea366@gmail.com>
2020-03-23 11:02:49 +0000

    created walk and feed methods

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -51,12 +51,50 @@ describe('constructor', () => {
         expect(pet.hunger).toEqual(5);
       });
     
-    it('decrements the fitness by 3', () => {
+     it('decrements the fitness by 3', () => {
         const pet = new Pet('Fido');
     
         pet.growUp();
     
         expect(pet.fitness).toEqual(7);
       });
+  });
+
+  describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('fido');
+  
+      pet.fitness = 4;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness to a maximum of 10', () => {
+      const pet = new Pet('fido');
+  
+      pet.fitness = 8;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+
+  describe('feed', () => {
+    it('decreases hunger by 3', () => {
+      const pet = new Pet('fido');
+  
+      pet.hunger = 5;
+      pet.feed();
+  
+      expect(pet.hunger).toEqual(2);
+    });
+    it('decreases hunger to a minimum of 0', () => {
+      const pet = new Pet('fido');
+  
+      pet.hunger = 2;
+      pet.feed();
+  
+      expect(pet.hunger).toEqual(0);
+    });
   });
 
--- src/pet.js
+++ src/pet.js
@@ -1,13 +1,38 @@
+const AGE_INCREMENT = 1; 
+
+const HUNGER_INCREMENT = 5;
+const HUNGER_DECREMENT = 3;
+const MINIMUM_HUNGER = 0;
+
+const FITNESS_DECREMENT = 3;
+const FITNESS_INCREMENT = 4;
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
 } 
 Pet.prototype.growUp = function() {
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
+    this.age += AGE_INCREMENT;
+    this.hunger += HUNGER_INCREMENT;
+    this.fitness -= FITNESS_DECREMENT;
 };
+Pet.prototype.walk = function() {
+   if((this.fitness + FITNESS_INCREMENT) <= MAXIMUM_FITNESS){
+    this.fitness += FITNESS_INCREMENT;
+   } else {
+    this.fitness = MAXIMUM_FITNESS;
+   }
+};
+Pet.prototype.feed = function() {
+    if ((this.hunger - HUNGER_DECREMENT) >= MINIMUM_HUNGER){
+    this.hunger -= HUNGER_DECREMENT;
+    } else {
+        this.hunger = MINIMUM_HUNGER;
+    }
+
+}
 
 module.exports = Pet;
